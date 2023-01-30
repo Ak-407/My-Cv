1ffb8db4f7e91dae90fb83fedbf90b113466add3
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-01-30 15:56:50 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import First from './components/first';
+import Head from './components/head';
+import Middle from './components/middle';
+import Middle2 from './components/middle2';
+import Middle3 from './components/middle3';
+import Middle4 from './components/middle4';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+ReactDOM.render(
+
+<div>
+  
+<First />
+<Head />
+<Middle />
+<Middle3 />
+<Middle2 />
+<Middle4 />
+
+
+ 
+
+</div>
+, document.getElementById("root"));
